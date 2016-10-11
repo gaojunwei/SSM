465ef1ca4b0d7dc9c2508baacd1742661d6f94bf
高军威 <gjw.5@qq.com>
2016-10-11 17:26:38 +0800

    测试阶段3

---
+++ WEB-INF/ssz_test/com/ssz/test/TestMyBatis.java
@@ -0,0 +1,41 @@
+package com.ssz.test;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSON;
+import com.ssz.service.IUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
+
+public class TestMyBatis {
+	private static Logger logger = Logger.getLogger(TestMyBatis.class);
+	@Resource
+	private IUserService userService = null;
+
+	/*private ApplicationContext ac = null;
+	@Before
+	public void before() {
+		ac = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+		userService = (IUserService) ac.getBean("userService");
+	}*/
+
+	@Test
+	public void test1() throws Exception {
+		Map<String, Object> rMap = userService.getUserByCon(" and cu_loginid='dongcunrui89'");
+		// System.out.println(user.getUserName());
+		// logger.info("值："+user.getUserName());
+		logger.info(JSON.toJSONString(rMap));
+	}
+}
