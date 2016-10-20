546251d431e0190c53fe44a4a58ae8b800e3b648
高军威 <gjw.5@qq.com>
2016-10-20 16:11:24 +0800

    返回json数据 功能添加 http://192.168.1.133:8080/ssm/web/getjson.do?userName=dongcunrui89

--- WEB-INF/ssz_src/com/ssz/controller/TestAction.java
+++ WEB-INF/ssz_src/com/ssz/controller/TestAction.java
@@ -1,31 +1,32 @@
 package com.ssz.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ssz.service.IUserService;
 
 @Controller
-@RequestMapping(value="/web")
+@RequestMapping(path="/web")
 public class TestAction {
 	
 	@Resource(name="userService")
 	private IUserService userService;
 	
-	@RequestMapping(value="/world",method=RequestMethod.GET)
+	@RequestMapping(path="/world",method=RequestMethod.GET)
 	public String hello(Model model)
 	{
 		model.addAttribute("msg", "您好！");
@@ -40,7 +41,7 @@ public class TestAction {
 	 * @throws Exception 
 	 * @author：gjw
 	 */
-	@RequestMapping(value="/login",method=RequestMethod.GET)
+	@RequestMapping(path="/login",method=RequestMethod.GET)
 	public ModelAndView login(String userName,String userPwd) throws Exception
 	{
 		String strCon = " and cu_loginid='"+userName+"'";
@@ -59,7 +60,7 @@ public class TestAction {
 	 * 		String
 	 * @author：gjw
 	 */
-	@RequestMapping(value="/tologin",method=RequestMethod.GET)
+	@RequestMapping(path="/tologin",method=RequestMethod.GET)
 	public String toLogin(Model model)
 	{
 		return "index/login";
@@ -73,25 +74,67 @@ public class TestAction {
 	 * @author：gjw
 	 */
 	@RequestMapping(value="/fileup",method=RequestMethod.POST)
-	public String upfile(Model model,String filename,File file_param)
+	public ModelAndView upfile(String filename,@RequestParam("file_param")MultipartFile file)
 	{
-		
-		
-		String realpath="c:/ce";
-		String extendsname=filename.substring(filename.lastIndexOf(".")); //截取.后面的名字如（jpg,gif...）
-		if (!(extendsname.equalsIgnoreCase(".jpg") || extendsname.equalsIgnoreCase(".gif")||extendsname.equalsIgnoreCase("png")||extendsname.equalsIgnoreCase("jpeg"))) {
-			model.addAttribute("msg", "输入格式不正确哦");
-			return "index/login";
+		ModelAndView model = new ModelAndView();
+		if(file.isEmpty())
+		{
+			model.addObject("msg", "请选择要上传的文件");
+			model.setViewName("index/login");
+			return model;
 		}
-		String filenames = UUID.randomUUID().toString()+new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())+extendsname;//将截取到得图片用uuid+时间组成新的名字;
+		String extendsname=file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")); //截取.后面的名字如（jpg,gif...）
+		if (!(extendsname.equalsIgnoreCase(".jpg") || extendsname.equalsIgnoreCase(".gif")||extendsname.equalsIgnoreCase("png")||extendsname.equalsIgnoreCase("jpeg"))) {
+			model.addObject("msg", "输入格式不正确哦");
+			model.setViewName("index/login");
+			return model;
+		}
 		
-		File destFile=new File(realpath,filenames);
+		File file_ = new File("c:/ce/aaa"+extendsname);
+		FileOutputStream fileOutStream = null;
+		byte[] bytes;
 		try {
-			FileUtils.copyFile(file_param, destFile);
+			bytes = file.getBytes();
+			fileOutStream = new FileOutputStream(file_);
+			fileOutStream.write(bytes,0,bytes.length); 
+			
 		} catch (IOException e) {
 			e.printStackTrace();
+			model.addObject("msg", "文件保存-异常1");
+			model.setViewName("index/login");
+			return model;
+		}finally
+		{
+			try {
+				fileOutStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				model.addObject("msg", "文件保存-异常2");
+				model.setViewName("index/login");
+				return model;
+			}
 		}
-		model.addAttribute("msg", "输入格式不正确哦");
-		return "index/login";
+		
+		model.addObject("msg", "文件保存成功");
+		model.setViewName("index/login");
+		return model;
+	}
+	/**
+	 * <li>功能名称：返回json数据</li>
+	 * <li>参数说明：</li>
+	 * <li>@param userName
+	 * <li>@param userPwd
+	 * <li>@return
+	 * <li>@throws Exception </li>
+	 * 		ModelAndView
+	 * @author：gjw
+	 */
+	@ResponseBody
+	@RequestMapping(path="/getjson",produces="application/json;charset=UTF-8")
+	public Map<String,Object> getJson(String userName) throws Exception
+	{
+		String strCon = " and cu_loginid='"+userName+"'";
+		Map<String, Object> rMap = this.userService.getUserByCon(strCon);
+		return rMap;
 	}
 }
